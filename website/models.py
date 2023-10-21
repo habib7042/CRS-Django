bd68329f56dd6923b69dd339a82fb092ecf7f1e4
habib7042 <ahasanh303@gmail.com>
2023-10-21 23:46:37 +0600

    update models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Models
+
+
+class UserData(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    zipcode = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
